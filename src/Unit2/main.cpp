af1b81d35b390a02837e2298ea9944d2278eb269
rinsato <rin_sato@akane.waseda.jp>
2025-03-22 13:54:25 +0900

    Refactor QuickSort implementation to use partitioning and update main function for sorting array

--- src/Unit2/main.cpp
+++ src/Unit2/main.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <array>
-void QuickSort(){
-    std::array<int, 9> arr = {3, 4, 13, 6, 3, 2, 9, 5, 7};
-    long n = arr.size();
 
-    for (int i = 0; i < n; i++)
+int Partition(std::array<int, 9> &arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        if (arr[j] < pivot)
         {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+
+void QuickSort(std::array<int, 9> &arr, int low, int high){
+    if (low < high)
+    {
+        int pi = Partition(arr, low, high);
+        QuickSort(arr, low, pi - 1);
+        QuickSort(arr, pi + 1, high);
+    }
+}
+int main()
+{
+    std::array<int, 9> arr = {3, 4, 13, 6, 3, 2, 9, 5, 7};
+    int n = arr.size();
+    QuickSort(arr, 0, n - 1);
     for (int i = 0; i < n; i++)
     {
         std::cout << arr[i] << " ";
     }
     std::cout << std::endl;
-}
-int main()
-{
-    QuickSort();
     return 0;
 }
